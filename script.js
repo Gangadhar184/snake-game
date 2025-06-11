21938ffbd46edc4e0f8995b5e04e57e127635cbe
Gangadhar184 <zenistuig65@gmail.com>
2025-06-12 00:33:08 +0530

    basic setup of game intializing snake, movement and food position

--- script.js
+++ script.js
@@ -1,28 +1,15 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const gameArena = document.querySelector(
-        '.game-arena'
-    )
-    const arenaSize = 600;
-    const cellSize = 20;
-    let score = 0;
-    let gameStarted = false;
-    let food = {x : 300, y : 200};
-    let snake = [{x : 160, y : 200}, {x: 140, y : 200}, {x: 120, y: 200}]   ;
-    let dx = cellSize //displacement on x-axis
-    let dy = 0;
+const gameArena = document.querySelector('.game-arena');
+const arenaSize = 600;
+const cellSize = 20;
+let score = 0;
+let gameStarted = false;
 
+//snake(array of segments)
+let snake = [{x:160, y: 200}, {x:140, y: 200}, {x: 120, y: 200}];
 
-    function startGame() {
-        const scoreBoard = document.createElement('div');
-        scoreBoard.id = 'score-board';
-        
-        document.body.insertBefore(scoreBoard, gameArena);
+//initialize movement direction
+let dx = cellSize;
+let dy = 0;
 
-
-        const startButton = document.createElement('button');
-        startButton.textContent = 'Start Game';
-        startButton.classList.add('start-button');
-        document.body.appendChild(startButton);
-    }
-    startGame();
-})
+//food position
+let food = {x : 300, y : 200};
